542fc280d98701ece187fedb9fae63b6da5b4f1a
Justin Detjen <jdetjen@gmx.com>
2019-12-07 17:16:38 -0600

    Refactor reset code

--- colorGame.js
+++ colorGame.js
@@ -6,53 +6,53 @@ let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.querySelector("#reset");
-let easyBtn = document.querySelector("#easyBtn");
-let hardBtn = document.querySelector("#hardBtn");
+let modeButtons = document.querySelectorAll(".mode");
+
+for(let i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function() {
+        modeButtons[0].classList.remove("selected")
+        modeButtons[1].classList.remove("selected");
+        this.classList.add("selected");
+
+        this.textContent === "Easy" ? numSquares = 3: numSquares = 6
+        // if(this.textContent === "Easy") {
+        //     numSquares = 3;
+        // } else {
+        //     numSquares = 6;
+        // }
+        reset();
 
 
-easyBtn.addEventListener("click", function() {
-    hardBtn.classList.remove("selected");
-    easyBtn.classList.add("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i = 0; i < squares.length; i++) {
-        if(colors[i]) {
-            squares[i].style.backgroundColor = colors[i];
-        } else {
-            squares[i].style.display = "none";
-        }
-    }
-});
 
-hardBtn.addEventListener("click", function() {
-    easyBtn.classList.remove("selected");
-    hardBtn.classList.add("selected");
-    numSquares = 6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "block";
-    }
-});
+    })
+}
 
-resetButton.addEventListener("click", function() {
-    //generate all new colors
+function reset() {
     colors = generateRandomColors(numSquares);
     //pick a new random color from array
     pickedColor = pickColor();
     //change colorDisplay to match picked color
     colorDisplay.textContent = pickedColor;
-    this.textContent = "New Colors";
+    resetButton.textContent = "New Colors";
     messageDisplay.textContent = "";
     //change colors of squares
     for(let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
+        if(colors[i]) {
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+
+
+
+        
     }
     h1.style.backgroundColor = "steelblue";
+}
+
+resetButton.addEventListener("click", function() {
+    reset();
 })
 
 colorDisplay.textContent = pickedColor;
